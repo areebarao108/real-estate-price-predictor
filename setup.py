f039987ccc51efbbfef4979543e2b73d8f11e886
Areeba Rao <areebarao108@gmail.com>
2026-02-27 22:42:50 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='real estate price prediction and recommendation project',
+    author='areeba rao',
+    license='',
+)
