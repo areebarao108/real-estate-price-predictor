beef3942eb25f7a59d6b64529c1ed3bded8fae3b
Areeba Rao <areebarao108@gmail.com>
2026-07-25 22:54:42 +0530

    update

--- pages/3_recommend apartments.py
+++ pages/3_recommend apartments.py
@@ -38,7 +38,7 @@ def recommend_properties_with_scores(property_name, top_n=5):
     return recommendations_df
 
 
-st.dataframe(location_df)
+
 
 st.title('Selcet location and radius')
 selected_location=st.selectbox('Location',sorted(location_df.columns.to_list()))
